cf798cae3727b5115fa2d9321a270c010a34f096
Lance Hirsch <lxirsh@gmail.com>
2015-12-01 16:34:59 +0200

    Implement a basic collection view.

---
+++ MemeMe 1.0/SentMemesCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SentMemesCollectionViewCell.swift
+//  MemeMe 1.0
+//
+//  Created by Lance Hirsch on 12/1/15.
+//  Copyright © 2015 Lance Hirsch. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memeImage: UIImageView!
+}
